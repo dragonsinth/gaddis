ee2b629034b15ea26f5b9dcf22c73fa05785ebd3
Scott Blum <dragonsinth@gmail.com>
2025-03-10 12:47:28 -0400

    set variable expression

--- cmd/gaddis/debug.go
+++ cmd/gaddis/debug.go
@@ -9,6 +9,7 @@ import (
 	"github.com/dragonsinth/gaddis/asm"
 	"github.com/dragonsinth/gaddis/ast"
 	"github.com/dragonsinth/gaddis/debug"
+	"github.com/dragonsinth/gaddis/parse"
 	"github.com/google/go-dap"
 	"io"
 	"log"
@@ -685,12 +686,12 @@ func (h *connHandler) onScopesRequest(request *dap.ScopesRequest) {
 	frameId := request.Arguments.FrameId
 	h.sess.GetStackFrames(func(fr *asm.Frame, id int, inst asm.Inst) {
 		si := fr.Scope.SourceInfo
-		varId := id * 1024
+		scopeId := id * 1024
 		if fr.Scope.IsGlobal {
 			response.Body.Scopes = append(response.Body.Scopes, dap.Scope{
 				Name:               "Globals",
 				PresentationHint:   "globals",
-				VariablesReference: varId,
+				VariablesReference: scopeId,
 				NamedVariables:     len(fr.Locals),
 				IndexedVariables:   0, // should also be len?
 				Expensive:          false,
@@ -704,7 +705,7 @@ func (h *connHandler) onScopesRequest(request *dap.ScopesRequest) {
 			response.Body.Scopes = append(response.Body.Scopes, dap.Scope{
 				Name:               "Locals",
 				PresentationHint:   "locals",
-				VariablesReference: varId,
+				VariablesReference: scopeId,
 				NamedVariables:     len(fr.Locals),
 				IndexedVariables:   0, // should also be len?
 				Expensive:          false,
@@ -717,7 +718,7 @@ func (h *connHandler) onScopesRequest(request *dap.ScopesRequest) {
 			response.Body.Scopes = append(response.Body.Scopes, dap.Scope{
 				Name:               "Arguments",
 				PresentationHint:   "arguments",
-				VariablesReference: varId + 1,
+				VariablesReference: scopeId + 512,
 				NamedVariables:     len(fr.Args),
 				IndexedVariables:   0, // should also be len?
 				Expensive:          false,
@@ -738,19 +739,20 @@ func (h *connHandler) onVariablesRequest(request *dap.VariablesRequest) {
 		return
 	}
 	varId := request.Arguments.VariablesReference
-	frameId := varId / 1024
-	varType := varId % 1024
+	scopeId := varId / 1024
+	isParamScope := varId&512 != 0
+	_ = varId % 512 // TODO
 	response := &dap.VariablesResponse{}
 	response.Response = *newResponse(request.Seq, request.Command)
 
-	addVar := func(val any, vd *ast.VarDecl) {
+	addVar := func(val any, vd *ast.VarDecl, id int) {
 		response.Body.Variables = append(response.Body.Variables, dap.Variable{
 			Name:               vd.Name,
 			Value:              asm.DebugStringVal(val),
 			Type:               vd.Type.String(),
 			PresentationHint:   nil,
 			EvaluateName:       vd.Name,
-			VariablesReference: 0, // list/map
+			VariablesReference: 0, // TODO: map/list
 			NamedVariables:     0, // map
 			IndexedVariables:   0, // list
 			MemoryReference:    "",
@@ -758,21 +760,20 @@ func (h *connHandler) onVariablesRequest(request *dap.VariablesRequest) {
 	}
 
 	h.sess.GetStackFrames(func(fr *asm.Frame, id int, inst asm.Inst) {
-		if id != frameId {
+		if id != scopeId {
 			return
 		}
-		switch varType {
-		case 0: // locals
+		if isParamScope {
+			for i, vd := range fr.Scope.Params {
+				addVar(fr.Args[i], vd, i)
+			}
+		} else {
 			nArgs := len(fr.Args)
 			for i, vd := range fr.Scope.Params {
-				addVar(fr.Locals[i], vd)
+				addVar(fr.Locals[i], vd, i)
 			}
 			for i, vd := range fr.Scope.Locals {
-				addVar(fr.Locals[i+nArgs], vd)
-			}
-		case 1: // args
-			for i, vd := range fr.Scope.Params {
-				addVar(fr.Args[i], vd)
+				addVar(fr.Locals[i+nArgs], vd, i)
 			}
 		}
 	})
@@ -780,8 +781,85 @@ func (h *connHandler) onVariablesRequest(request *dap.VariablesRequest) {
 }
 
 func (h *connHandler) onSetVariableRequest(request *dap.SetVariableRequest) {
-	// TODO: support
-	h.send(newErrorResponse(request.Seq, request.Command, "setVariableRequest is not yet supported"))
+	name := request.Arguments.Name
+	value := request.Arguments.Value
+	varId := request.Arguments.VariablesReference
+	scopeId := varId / 1024
+	isParamScope := varId&512 != 0
+	_ = varId % 512 // TODO
+
+	var err error
+	var typStr string
+	var valStr string
+	h.sess.GetStackFrames(func(fr *asm.Frame, id int, inst asm.Inst) {
+		if id != scopeId {
+			return
+		}
+
+		decl, ref := func() (*ast.VarDecl, *any) {
+			if isParamScope {
+				for i, vd := range fr.Scope.Params {
+					if vd.Name == name {
+						return vd, &fr.Args[i]
+					}
+				}
+			} else {
+				nArgs := len(fr.Args)
+				for i, vd := range fr.Scope.Params {
+					if vd.Name == name {
+						return vd, &fr.Locals[i]
+					}
+				}
+				for i, vd := range fr.Scope.Locals {
+					if vd.Name == name {
+						return vd, &fr.Locals[i+nArgs]
+					}
+				}
+			}
+			return nil, nil
+		}()
+
+		if decl == nil {
+			err = fmt.Errorf("unknown variable %s", name)
+			return
+		}
+		if !decl.Type.IsPrimitive() {
+			err = fmt.Errorf("variable %s of type %s is not primitive", name, decl.Type)
+			return
+		}
+
+		var val any
+		if value == "<nil>" {
+			val = nil
+		} else if lit := parse.ParseLiteral(value, ast.SourceInfo{}, decl.Type.AsPrimitive()); lit != nil {
+			val = lit.Val
+			if str, ok := val.(string); ok {
+				val = []byte(str)
+			}
+		} else {
+			err = fmt.Errorf("failed to parse value %q of type %s", value, decl.Type)
+			return
+		}
+
+		*ref = val
+		typStr = decl.Type.String()
+		valStr = asm.DebugStringVal(val)
+	})
+	if err != nil {
+		h.send(newErrorResponse(request.Seq, request.Command, err.Error()))
+		return
+	}
+
+	response := &dap.SetVariableResponse{}
+	response.Response = *newResponse(request.Seq, request.Command)
+	response.Body = dap.SetVariableResponseBody{
+		Value:              valStr,
+		Type:               typStr,
+		VariablesReference: request.Arguments.VariablesReference,
+		NamedVariables:     0,
+		IndexedVariables:   0,
+	}
+	h.send(response)
 }
 
 func (h *connHandler) onSetExpressionRequest(request *dap.SetExpressionRequest) {
@@ -968,14 +1046,14 @@ func debugCmd(port int, verbose bool) error {
 	if port >= 0 {
 		return debugServer(port, dbgLog)
 	} else {
-		debugSession := connHandler{
+		ch := connHandler{
 			rw:        bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)),
 			sendQueue: make(chan dap.Message),
 			bps:       map[string][]int{},
 			dbgLog:    dbgLog,
 		}
 
-		return debugSession.Run()
+		return ch.Run()
 	}
 }
 
