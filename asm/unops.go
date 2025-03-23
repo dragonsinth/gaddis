7817b148f762c9cbe9374c538776c9a7a4aa0c46
Scott Blum <dragonsinth@gmail.com>
2025-03-23 14:25:31 -0400

    cleanup, no more for.go -> unops for incr

--- asm/unops.go
+++ asm/unops.go
@@ -3,6 +3,7 @@ package asm
 import (
 	"fmt"
 	"github.com/dragonsinth/gaddis/ast"
+	"strconv"
 )
 
 type UnaryOpInt struct {
@@ -60,4 +61,34 @@ func (i UnaryOpBool) Exec(p *Execution) {
 
 func (i UnaryOpBool) String() string {
 	return i.Op.Name()
+}
+
+type IncrInt struct {
+	baseInst
+	Val int64
+}
+
+func (i IncrInt) Exec(p *Execution) {
+	ref := p.Pop().(*any)
+	refVal := (*ref).(int64)
+	*ref = refVal + i.Val
+}
+
+func (i IncrInt) String() string {
+	return "incr int " + strconv.FormatInt(i.Val, 10)
+}
+
+type IncrReal struct {
+	baseInst
+	Val float64
+}
+
+func (i IncrReal) Exec(p *Execution) {
+	ref := p.Pop().(*any)
+	refVal := (*ref).(float64)
+	*ref = refVal + i.Val
+}
+
+func (i IncrReal) String() string {
+	return "incr real " + strconv.FormatFloat(i.Val, 'g', -1, 64)
 }
